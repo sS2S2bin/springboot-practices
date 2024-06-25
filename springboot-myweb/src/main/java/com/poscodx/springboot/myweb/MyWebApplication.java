544388809c9b8c3eeb85cef30db0557bb1217387
sS2S2bin <3dltnqls@naver.com>
2024-06-25 11:11:21 +0900

    init: springboot-myweb

---
+++ springboot-myweb/src/main/java/com/poscodx/springboot/myweb/MyWebApplication.java
@@ -0,0 +1,11 @@
+package com.poscodx.springboot.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebApplication {
+	public static void main(String[] args) {
+		 SpringApplication.run(MyWebApplication.class,args);
+	}
+}
